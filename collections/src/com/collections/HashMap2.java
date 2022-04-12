9d9a3887a8a602580bc384b5ad5087d0b076b341
Dhanush.S <Dhanush.S@EXIND3010-32.allfon.in>
2022-04-12 12:31:35 +0530

    Updated Collection

---
+++ collections/src/com/collections/HashMap2.java
@@ -0,0 +1,45 @@
+package com.collections;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMap2 {
+	public static void main(String[] args) {
+		
+		
+		Student ram = new Student(101, "dhanush", 80);
+		Student shazam = new Student(102, "shazam", 70);
+		Student jagan = new Student(106, "jagan", 76);
+		Student harish = new Student(007, "harish", 45);
+		
+		Map<Integer,Student> map = new HashMap<>();
+		
+		map.put(900, ram);
+		map.put(901, shazam);
+		map.put(902, jagan);
+		
+		Set<Integer> keys=map.keySet();
+		
+		for(Integer key : keys) {
+			System.out.println("Getting value using key "+map.get(key));
+		}
+		
+		Collection<Student> list = map.values();
+
+		for(Student student:list) {
+			System.out.println("Getting value using value "+student);
+		}
+		
+		Set<Map.Entry<Integer, Student>> list1= map.entrySet();
+		System.out.println("Getting Both key and value");
+		for(Map.Entry<Integer, Student> eachValue : list1) {
+	
+			System.out.println(eachValue.getKey());
+			System.out.println(eachValue.getValue());
+		}
+		
+	}
+}
--- collections/src/com/collections/HashMapApplication.java
+++ collections/src/com/collections/HashMapApplication.java
@@ -11,22 +11,30 @@ public class HashMapApplication {
 		Student shazam = new Student(102, "shazam", 70);
 		Student jagan = new Student(106, "jagan", 76);
 		Student harish = new Student(007, "harish", 45);
-			
-		Map<Integer,Student> map = new HashMap<>();
 		
-		map.put(500, ram);
+		Map<Integer,Student> map = new HashMap<>();
+			   
+				
+		 map.put(500, ram);
+				  
 		map.put(501, shazam);
 		
-		System.out.println(map.put(502, jagan));
-
-		System.out.println(map.put(502, harish));
+		map.put(502, jagan); //null
+		
+		Student added=map.put(502, harish); //original
+		
+		if(added!=null){
+			map.put(503, added);
+		}
 		
 		System.out.println(map.get(502));
+		System.out.println(map.get(503));
 		
 //		for(Map.Entry<Integer, Student> map1 : map.entrySet()) {
 //			int key=map1.getKey();
 //			Student value=map1.getValue();
 //			System.out.println("Key -> "+key +" value -> "+value);
 //		}
+		
 	}
 }
