c35020b5d25e12cdb6b3c0f478f6fe5a20f3a7d1
Dhanush.S <Dhanush.S@EXIND3010-32.allfon.in>
2022-04-12 15:48:09 +0530

    HashSet Function

--- assessment4/src/com/training/Application.java
+++ assessment4/src/com/training/Application.java
@@ -1,10 +1,60 @@
 package com.training;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.print.Doc;
+
+import com.training.model.Doctor;
+import com.training.model.Patient;
+import com.training.services.Appointment;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Patient ramesh= new Patient(101, "ramesh", "anna nagar", 1233242422l);
+		Patient gokul= new Patient(102, "gokul", "t.nagar", 53532532532l);
+		Patient magesh = new Patient(103, "magesh", "ECR", 5362636262662l);
+		Patient magesh1 = new Patient(103, "magesh", "ECR", 5362636262662l);
+		Patient dhanush	= new Patient(104, "dhanush", "ayanavaram", 9791015987l);
 		
-		System.out.println(args[0]+" "+args[1]);
+		Set<Patient> patients = new HashSet<Patient>();
+		
+		patients.add(ramesh);
+		patients.add(gokul);
+		patients.add(magesh);
+		
+		
+		Set<Patient> patients2 = new HashSet<>();
+		
+		patients2.add(magesh1);
+		patients2.add(dhanush);
+		
+		Doctor doctor= new Doctor(900, "jithin", "Cardiologist", patients);
+		Doctor doctor2= new Doctor(800, "sidharth", "Tumor Funtionalist", patients2);
+		Set<Doctor> doctors = new HashSet<Doctor>();
+		
+		doctors.add(doctor);
+		doctors.add(doctor2);
+		
+//		for(Doctor doc : doctors) {
+//			System.out.println(doc.getPatients()+"\n");			
+//		}		
+		
+		Map<Doctor,Set<Patient>> map = new HashMap<Doctor,Set<Patient>>();
+		
+		Appointment appointment= new Appointment(map);
+		System.out.println("Map from here");
+		for(Map.Entry<Doctor, Set<Patient>> eachValue : appointment.getMap().entrySet()) {
+			
+			if(eachValue.getKey().getDoctorId()==900) {
+				
+				System.out.println(eachValue.getValue());
+			}
+		}
+		
 	}
 }
---
+++ assessment4/src/com/training/services/Appointment.java
@@ -0,0 +1,55 @@
+package com.training.services;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.training.model.*;
+
+public class Appointment {
+	private Map<Doctor,Set<Patient>> map;
+
+	public Appointment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Map<Doctor, Set<Patient>> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Doctor, Set<Patient>> map) {
+		this.map = map;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((map == null) ? 0 : map.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Appointment other = (Appointment) obj;
+		if (map == null) {
+			if (other.map != null)
+				return false;
+		} else if (!map.equals(other.map))
+			return false;
+		return true;
+	}
+
+	public Appointment(Map<Doctor, Set<Patient>> map) {
+		super();
+		this.map = map;
+	}	
+	
+	
+}
